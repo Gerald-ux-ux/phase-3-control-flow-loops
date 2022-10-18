476e8fbc8dfd33245c645ad62985fc8b70f22536
Gerald <gerald.kamau@student.moringaschool.com>
2022-10-18 17:25:53 +0300

    completed assignment

---
+++ gerald.rb
@@ -0,0 +1,7 @@
+# def reverse_string(str)
+#   reversed_str = ""
+#   str.length.times do |i|
+#     reversed_str = str[i] + reversed_str
+#   end
+#   reversed_str
+# end
--- looping.rb
+++ looping.rb
@@ -1,6 +1,16 @@
+
 def happy_new_year
-  # your code here
+ counter = 10 
+  until counter == 0 
+   puts counter
+   counter -=1
+   end
+   puts "Happy New Year!"
 end
+
+puts happy_new_year
+
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -16,9 +26,26 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  (1..100).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+    end
 end
 
 def reverse_string(str)
-  # your code here
+  split_string = str.split("")
+  reversed = []
+  str.size.times { reversed << split_string.pop }
+  reversed.join
 end
+
+ puts reverse_string ("Gerald")
+
+
